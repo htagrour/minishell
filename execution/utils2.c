336080add2e4631817beab9575958e642deb0f3f
Fatima zahra Sarbout <fsarbout@e2r10p12.1337.ma>
2021-03-12 15:31:42 +0100

    echo -n working without double quote

--- execution/utils2.c
+++ execution/utils2.c
@@ -6,7 +6,7 @@
 /*   By: fsarbout <fsarbout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 22:20:35 by fsarbout          #+#    #+#             */
-/*   Updated: 2021/03/12 15:22:13 by fsarbout         ###   ########.fr       */
+/*   Updated: 2021/03/12 15:31:38 by fsarbout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	ft_strchr_n(const char *s, int c)
 }
 
 void	echo_check_n(char **args, int *flag)
-{
+{ 
     int i = lenght(args);
     while (i > 0)
     {
